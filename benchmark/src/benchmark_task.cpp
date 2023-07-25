760d9115de89b8a8efbfc83fda38249bf73f9fcf
Barry Robinson <barry.w.robinson64@gmail.com>
2023-07-25 23:34:40 +0100

    add basic benchmark

--- benchmark/src/benchmark_task.cpp
+++ benchmark/src/benchmark_task.cpp
@@ -1,32 +1,31 @@
 #include <benchmark/benchmark.h>
 #include <libProducerConsumer.h>
 
-/**
- * @file benchmark_task.cpp
- * @author Barry Robinson (barry.w.robinson64@gmail.com)
- * @brief Test harness for task application library
- * @version 0.1
- * @date 11/10/2020
- *
- * @copyright Copyright (c) 2021
- * @addtogroup benchmark
- * @{
- * @addtogroup main
- * @{
- *
- */static void BM_RunFullParser(benchmark::State &state) {
-    for (auto _ : state) {
-    }
-}
+#include <libProducerConsumer.h>
 
-static void BM_RunSimpleParser(benchmark::State &state) {
-    for (auto _ : state) {
+static void BM_AddGetBuffer(benchmark::State &state) {
+    ring_buffer_t rb;
+    ring_buffer_err_t er = rb_init(&rb,256);
+    if(er != RB_ERR_OK) {
+        printf("failed to create ring buffer\n");
+        abort();
     }
+
+    for (auto _ : state) {
+        for(int i=0; i<256; i++) {
+            rb_add(&rb,i);
+        }
+
+        for(int i=0; i < 256; i++) {
+            rb_get(&rb);
+        }
+    }
+
+    rb_teardown(&rb);
 }
 
 // Register the function as a benchmark
-BENCHMARK(BM_RunFullParser);
-BENCHMARK(BM_RunSimpleParser);
+BENCHMARK(BM_AddGetBuffer);
 
 // Run the benchmark
 BENCHMARK_MAIN();
