b8af0af89b761d5038cec03155b5776798b9aa6a
Barry Robinson <barry.w.robinson64@gmail.com>
2023-07-26 19:34:26 +0100

    remove comments from main

--- test/src/test_main.cpp
+++ test/src/test_main.cpp
@@ -1,21 +1,6 @@
-/**
- * @file test_parser.cpp
- * @author Barry Robinson (barry.w.robinson64@gmail.com)
- * @brief Test harness for filescan application library
- * @version 0.1
- * @date 26/09/2021
- *
- * @copyright Copyright (c) 2022
- * @addtogroup test
- * @{
- * @addtogroup main
- * @{
- *
- */
-
-
 #include <gtest/gtest.h>
 #include <glog/logging.h>
+
 /**
  * @brief Main function for test harness
  *
@@ -30,7 +15,3 @@ int main(int argc, const char ** argv) {
 
 }
 
-/**
- * @}
- * @}
- */
